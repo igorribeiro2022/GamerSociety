27f16647f1926ca82670bc8f8ec9d4241bb83088
GusBTB <gustavocs81@gmail.com>
2022-11-07 14:37:17 -0300

    feat: retrieve game bet

--- bets/views.py
+++ bets/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .serializers import BetSerializer
+from games.models import Game
+from rest_framework.exceptions import NotFound
+import uuid
+from bets.models import Bet
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+# class RetrieveBetGameView(generics.RetrieveAPIView):
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated]
+#     lookup_url_kwarg = "game_id"
+#     serializer_class = BetSerializer
+    
+    # def get_queryset(self):
+    #     game_id = self.kwargs['game_id']
+    #     try:
+    #         uuid.UUID(game_id, version=4)
+    #     except ValueError:
+
+    #         raise NotFound({"detail": "ID not UUID type"})
+
+    #     game = get_object_or_404(Game, id=self.kwargs['game_id'])
+    #     return Bet.objects.filter(game=game)
+    
+    # def get_object(self):
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #     bet = queryset[0]
+    #     return bet
+
+class RetrieveBetGameView(generics.RetrieveAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    lookup_url_kwarg = "game_id"
+    serializer_class = BetSerializer
+    queryset = Game.objects.all()
+    
+    def get_object(self): 
+        game_id = self.kwargs['game_id']
+        try:
+            uuid.UUID(game_id, version=4)
+        except ValueError:
+
+            raise NotFound({"detail": "ID not UUID type"})
+        game = get_object_or_404(Game, id=self.kwargs['game_id'])
+
+        bet = game.bet
+        return bet
--- games/urls.py
+++ games/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from user_bets.views import CreateUserBetView
+from bets.views import RetrieveBetGameView
 
 urlpatterns = [
     path("games/", views.ListBettableGamesView.as_view()),
     path("games/<str:game_id>/", views.UpdateTeamsGameView.as_view()),
     path("games/<str:game_id>/winner/", views.UpdateGameWinnerView.as_view()),
     path("games/<str:game_id>/bet/<str:team_id>/", CreateUserBetView.as_view() ),
+    path("games/<str:game_id>/bet/", RetrieveBetGameView.as_view() )
 ]
