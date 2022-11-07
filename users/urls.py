0784e2b962910fe027180218a0d1ecf3d4e52fd6
GusBTB <gustavocs81@gmail.com>
2022-11-07 03:44:22 -0300

    feat: addding documentation

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from users import views
-from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path("users/", views.ListUsersView.as_view()),
@@ -8,4 +8,6 @@ urlpatterns = [
     path("users/<str:user_id>/", views.RetrieveUpdateUserView.as_view()),
     path("users/<str:user_id>/activity/", views.UpdateActivityUserView.as_view()),
     path("login/", views.LoginView.as_view()),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("docs/", SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
