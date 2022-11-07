40c4250b43e34ce9721a87ad4d7e6a78cd4f8f3a
Pedro Lucas Xavier <pedrolucas.xs@gmail.com>
2022-11-07 15:05:54 -0300

    fix: branch rebased

--- teams/urls.py
+++ teams/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path("teams/", views.ListTeamsView.as_view()),
     path("teams/register/", views.CreateTeamsView.as_view()),
     path("teams/<str:pk>/", views.RetrieveUpdateDeleteTeams.as_view()),
-    path("teams/add/<str:pk>/", views.InsertUsersInTeams.as_view()),
-    path("teams/remove/<str:team_id>/champ/<str:championship_id>/", views.RemoveTeamFromChampionship.as_view())
+    path("teams/add/<str:pk>/", views.InsertUsersInTeams.as_view())
 ]
