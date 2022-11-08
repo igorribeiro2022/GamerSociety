c60c3b1c4d0cd37e3c8df889ba7088e7b40a3572
João Pedro Porto Teixeira <joao.porto.ismart@gmail.com>
2022-11-08 14:11:52 -0300

    feat: update team name initials

--- teams/serializers.py
+++ teams/serializers.py
@@ -3,6 +3,23 @@ from users.serializers import UserForTeamSerializer
 from .models import Team
 
 
+class TeamDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Team
+        fields = "__all__"
+        read_only_fields = [
+            "championship",
+            "users",
+            "id",
+            "wins",
+            "losses",
+            "e_sport",
+        ]
+
+    # championship = ListChampionshipsSerializer(read_only=True, allow_null=True)
+    users = UserForTeamSerializer(many=True, allow_null=True, read_only=True)
+
+
 class TeamSerializer(ModelSerializer):
     class Meta:
         model = Team
@@ -14,7 +31,6 @@ class TeamSerializer(ModelSerializer):
 
     def update(self, instance, validated_data):
         users_list = validated_data["users"]
-
 
         for user in users_list:
             instance.users.add(user)
