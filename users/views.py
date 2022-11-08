89464728b857069e4d9da5c5147f716de756607a
GusBTB <gustavocs81@gmail.com>
2022-11-08 13:55:43 -0300

    feat: user bet list

--- users/views.py
+++ users/views.py
@@ -7,12 +7,14 @@ from .serializers import (
     SuperUserCreateSerializer,
 )
 from .permissions import IsAdmin, IsOwnerOrAdmin, IsStaffOrAccountOwner
-
+from user_bets.models import UserBet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.views import Response, Request
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
+from user_bets.serializers import ListUserBetSerializer
 
 
 class ListUsersView(generics.ListAPIView):
@@ -21,6 +23,21 @@ class ListUsersView(generics.ListAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserListSerializer
+    
+class ListUserBetsView(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = UserBet.objects.all()
+    serializer_class = ListUserBetSerializer
+    
+    def list(self, request, *args, **kwargs):
+        
+        user_bets = UserBet.objects.filter(user=request.user.id)
+        
+        serializer = self.get_serializer(user_bets, many=True)
+        return Response(serializer.data)
+        
 
 
 class CreateUserView(generics.CreateAPIView):
